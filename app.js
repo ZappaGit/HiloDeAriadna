95c9de92f644ee27812ffe48d05aa44b25cd881b
ZappaGit <dgizquierdo@gmail.com>
2016-11-26 18:33:55 +0100

    Commit message

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
 var express = require('express');
+// Lo iniciamos
 var app = express();
-app.use(app.router);
+
+// Definimos la ruta principal
+app.get('/', function (req, res) {
+  // Obtenemos el nombre (no necesitamos el modulo url!)
+  var nombre = req.query;
+  res.send('<h1>' + 'Amor mio te quiero' + '</h1>');
+});
+
+// Decimos en que puerto queremos escuchar (el 8000)
+app.listen(8000, function () {
+  console.log("Esperando requests en el puerto 8000");
+});
